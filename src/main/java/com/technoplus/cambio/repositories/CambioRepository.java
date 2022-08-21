53e585bdc939306ef9424c74734c852af959b6d1
Sifisso <sifissom@gmail.com>
2022-08-21 08:15:33 +0200

    Criação do pacote dos repositórios e o respectivo repository

---
+++ src/main/java/com/technoplus/cambio/repositories/CambioRepository.java
@@ -0,0 +1,12 @@
+package com.technoplus.cambio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.technoplus.cambio.models.Cambio;
+
+@Repository
+public interface CambioRepository extends JpaRepository<Cambio, String>{
+
+	
+}
