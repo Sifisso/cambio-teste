99201178b00b10e3b9d8bed6c410cfc13ef3524a
Sifisso <sifissom@gmail.com>
2022-08-21 08:05:20 +0200

    Criação do pacote do serviços e o próprio serviço

--- src/main/java/com/technoplus/cambio/controllers/CambioController.java
+++ src/main/java/com/technoplus/cambio/controllers/CambioController.java
@@ -1,6 +1,5 @@
 package com.technoplus.cambio.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,11 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class CambioController {
 	
 	
-	@GetMapping("/")
-	public String Verificarcambio() {
-		
 	
-		return "cambio";
-	}
 
 }
---
+++ src/main/java/com/technoplus/cambio/services/CambioService.java
@@ -0,0 +1,9 @@
+package com.technoplus.cambio.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CambioService {
+	
+}
+	
